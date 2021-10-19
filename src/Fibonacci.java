919613bb2788cfb109c718f3ba03ba4850660ae8
Jen <jmiller283@mail.valenciacollege.edu>
2021-10-18 20:18:59 -0400

    add table

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -21,4 +21,33 @@ public class Fibonacci {
 		}
 		return r;
 	}
+	// table
+	public static void main (String[] args) {
+		int i, n = 40, f1, f2;
+		System.out.print("n\tIterative\tRecursive\n");
+		System.out.print("==\t=========\t=========\n");
+		long startTime, endTime;
+		int difference, difference2;
+		//loop from 20 to 40 
+		for (i = 20; i <= n; i++) { 
+			// initial time
+			startTime = System.nanoTime(); 
+			// fibonacci using iterative method
+			f1 = fibonaccii(i); 
+			// end time
+			endTime = System.nanoTime(); 
+			// difference
+			difference = (int)((endTime - startTime) / 1e6); 
+			// initial time
+			startTime = System.nanoTime(); 
+			// fibonacci using recursive method
+			f2 = fibonaccir(i); 
+			// end time
+			endTime = System.nanoTime(); 
+			// difference
+			difference2 = (int)((endTime - startTime) / 1e6); 
+			// output
+			System.out.print(i + "\t\t" + difference + "\t\t\t" + difference2 + "\t\n"); 
+		}
+	}
 }
