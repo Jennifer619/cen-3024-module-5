f81981158416eb6316eabcf06b154be97ed28242
Jen <jmiller283@mail.valenciacollege.edu>
2021-10-18 20:09:41 -0400

    add iterative method

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -8,4 +8,17 @@ public class Fibonacci {
 			return fibonaccir (n - 1) + fibonaccir (n - 2);
 		}
 	}
+	// iterative method
+	public static int fibonaccii (int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		int r = 0, p = 1, pp = 1, i;
+		for (i = 2; i <= n; i++) {
+			r = p + pp;
+			pp = p;
+			p = r;
+		}
+		return r;
+	}
 }
