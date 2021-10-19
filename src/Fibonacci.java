f4b31992960d1b64a8ef023a1fe30e717a0bb69a
Jen <jmiller283@mail.valenciacollege.edu>
2021-10-18 20:04:34 -0400

    add recursive method

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,4 +1,11 @@
 
 public class Fibonacci {
-
+	// recursive method
+	public static int fibonaccir (int n) { 
+		if (n <= 1) {
+			return 1;
+		} else {
+			return fibonaccir (n - 1) + fibonaccir (n - 2);
+		}
+	}
 }
